d35f72c969988172f3e4af213cd2bf755ce02dd4
William R <wiribasczky@gmail.com>
2020-11-18 16:58:25 -0300

    Criado processo de build
    Problema/Proposito
    - Criado processo de build utilizando o webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin({})],
+  },
+  entry: {
+    slide: './external/slide-dist.js',
+    slidenav: './external/slidenav-dist.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: 'slide.css' })],
+};
