55e913d1da76226f3cf0d499d8c2d6bc526c8486
William R <wiribasczky@gmail.com>
2020-11-18 16:56:36 -0300

    Separação da classe de slide com navegação
    Problema/Proposito
    - Separação das classes Slide e SlideNav em arquivos separados

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js';
 
-export class Slide {
+export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
@@ -10,7 +10,7 @@ export class Slide {
       movement: 0,
     };
     this.activeClass = 'active';
-    this.changeEvent = new Event('changeEvent')
+    this.changeEvent = new Event('changeEvent');
   }
 
   transition(active) {
@@ -149,58 +149,5 @@ export class Slide {
     this.addResizeEvent();
     this.changeSlide(0);
     return this;
-  }
-}
-
-export default class SlideNav extends Slide {
-  constructor(slide, wrapper) {
-    super(slide, wrapper);
-    this.bindControlEvents();
-  }
-
-  addArrow(prev, next) {
-    this.prevElement = document.querySelector(prev);
-    this.nextElement = document.querySelector(next);
-    this.addArrowEvent();
-  }
-
-  addArrowEvent() {
-    this.prevElement.addEventListener('click', this.activePrevSlide);
-    this.nextElement.addEventListener('click', this.activeNextSlide);
-  }
-
-  createControl() {
-    const control = document.createElement('ul');
-    control.dataset.control = 'slide';
-    this.slideArray.forEach((item, index) => {
-      control.innerHTML += `<li><a href="#slide${index + 1}">${index + 1}</a></li>`;
-    });
-    this.wrapper.appendChild(control);
-    return control;
-  }
-
-  eventControl(item, index) {
-    item.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.changeSlide(index);
-    });
-    this.wrapper.addEventListener('changeEvent', this.activeControlItem)
-  }
-
-  activeControlItem() {
-    this.controlArray.forEach((item) => item.classList.remove(this.activeClass));
-    this.controlArray[this.index.active].classList.add(this.activeClass);
-  }
-
-  addControl(customControl) {
-    this.control = document.querySelector(customControl) || this.createControl();
-    this.controlArray = [...this.control.children];
-    this.activeControlItem();
-    this.controlArray.forEach(this.eventControl);
-  }
-
-  bindControlEvents() {
-    this.eventControl = this.eventControl.bind(this);
-    this.activeControlItem = this.activeControlItem.bind(this);
   }
 }
